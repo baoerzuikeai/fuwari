651800277dda4a38f6fc91fdfd422447c937dd49
baoerzuikeai <baoerzuikeai@gmail.com>
2025-02-22 00:13:49 +0800

    fix: update base path in Astro configuration for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://baoerzuikeai.github.io",
-  base: "/",
+  base: "/fuwari",
   trailingSlash: "always",
   integrations: [
     tailwind(
